b381c1cfe6ebc2c4dcffe11167c46db7309c4f16
frolin28 <frolkin2801@gmail.com>
2023-02-04 21:49:09 +0200

    stream logs to stdout

--- hackaton/__main__.py
+++ hackaton/__main__.py
@@ -1,10 +1,14 @@
 if __name__ == '__main__':
+    import logging
+    import sys
+
     from aiohttp.web import run_app
     from uvloop import install
+
     from hackaton.app import make_app
     from hackaton.config import CONFIG
-    from logging import info
 
-    info('{} started'.format('hackaton'))
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    logging.info('{} started'.format('hackaton'))
     install()
     run_app(make_app(), port=CONFIG['port'])
