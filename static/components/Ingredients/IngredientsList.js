f2d913647a49d00a97be96fb7d4798e6fbedbe0d
frolin28 <frolkin2801@gmail.com>
2023-02-08 18:27:58 +0200

    logged in user functionality

--- static/components/Ingredients/IngredientsList.js
+++ static/components/Ingredients/IngredientsList.js
@@ -1,54 +1,78 @@
-import './style/IngredientsList.css';
-import React, {useEffect, useState} from 'react';
-import {Link} from 'react-router-dom';
-import LoadingPage from '../Pages/LoadingPage';
-import Page404 from '../Pages/Page404';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useQuery } from 'react-query';
 
-function IngredientsList(props)
-{
+import LoadingPage from '../Pages/LoadingPage';
+import { useUser } from '../../context/UserContext';
+
+import './style/IngredientsList.css';
+
+
+function IngredientsList(props) {
     const [ingredients, setIngredients] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    setTimeout(() => setLoading(false), 5000);
+    // setTimeout(() => setLoading(false), 5000);
     const [categoryTypes, setCategoryTypes] = useState([]);
     const [ingredientType, setIngredientType] = useState(null);
 
     const [page, setPage] = useState(1);
     const [nextPage, setNextPage] = useState(null);
+    const [showUser, setShowUser] = useState(false);
     const moreIngredientsHandler = () => setPage(page + 1);
-    const addIngredient = () => 1;
+    const { user } = useUser();
 
     const selectIngredients = e => {
         setIngredientType(e.target.value);
         setPage(1);
     }
 
+    const handleChangeShowUser = () => setShowUser(!showUser);
+
+    const addIngredient = (ingredientId) => {
+        fetch('/api/user/product', {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            body: JSON.stringify({ product_id: ingredientId })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.status !== 'success') return;
+                console.log('Successful added');
+            })
+    };
+
     useEffect(() => {
         setPage(1);
     }, props.searchWord)
 
     useEffect(() => {
-        fetch(`/api/ingredient/search`,{
+        fetch(`/api/ingredient/search`, {
             method: 'POST',
             mode: 'cors',
             cache: 'no-cache',
             credentials: 'same-origin',
-            body: JSON.stringify(
-                {page, ingredient_type: ingredientType, query: props.searchWord}
-            )
+            body: JSON.stringify({
+                page,
+                ingredient_type: ingredientType,
+                query: props.searchWord,
+                for_user: !!user && showUser
+            })
         })
             .then(response => response.json())
             .then(data => {
-                if(data.status !== 'success') return;
+                if (data.status !== 'success') return;
                 setLoading(false);
                 if (ingredientType && page === 1 || props.searchWord && page === 1) setIngredients(data.payload.ingredients);
                 else setIngredients([...ingredients, ...data.payload.ingredients]);
                 setNextPage(data.payload.nextPage);
             });
-    }, [page, ingredientType, props.searchWord]);
+    }, [page, ingredientType, props.searchWord, user, showUser]);
 
     useEffect(() => {
-        fetch(`/api/ingredient_types`,{
+        fetch(`/api/ingredient_types`, {
             method: 'GET',
             mode: 'cors',
             cache: 'no-cache',
@@ -56,48 +80,54 @@ function IngredientsList(props)
         })
             .then(response => response.json())
             .then(data => {
-                if(data.status !== 'success') return;
+                if (data.status !== 'success') return;
                 setCategoryTypes(data.payload);
             });
     }, []);
 
-    return(
+    return (
         <>
-        {loading === true ? (
-            <LoadingPage />
-        ) : (
-            <div className='home-page ingredients' id='ingredients'>
-                <div className='ingredients-declaration'>Search by Ingredient</div>
-                <select name="select-ingredients" onChange={selectIngredients}>
-                    <option value="">--Please choose the category--</option>
-                    {categoryTypes.map((categoryType) => (
-                        <option className="dropdown__option" key={categoryType.title}>{categoryType.title}</option>
-                    ))}
-                </select>
-                <div className='ingredient-list'>
-                    {ingredients.map((ingredient) => (
-                        <div className='ingredient' key={ingredient._id}>
-                            <Link to={{
-                                pathname:`/ingredients/${ingredient.title}`, state: { ingredient }
-                            }}>
-                                <img className='ingredient-thumbnail' src={`https://www.themealdb.com/images/ingredients/${ingredient.title}.png`} alt='T_T'/>
-                                <div className='ingredient-name'>{ingredient.title}</div>
-                            </Link>
-                            <div className='button-container'>
-                                <button className='recipe-but' onClick={addIngredient}>Add</button>
+            {loading === true ? (
+                <LoadingPage />
+            ) : (
+                <div className='home-page ingredients' id='ingredients'>
+                    <div className='ingredients-declaration'>Search by Ingredient</div>
+                    <select name="select-ingredients" onChange={selectIngredients}>
+                        <option value="">--Please choose the category--</option>
+                        {categoryTypes.map((categoryType) => (
+                            <option className="dropdown__option" key={categoryType.title}>{categoryType.title}</option>
+                        ))}
+                    </select>
+                    {user &&
+                        <label>
+                            <input type="checkbox" checked={showUser} onChange={handleChangeShowUser} />
+                            Show only recipes available for user
+                        </label>
+                    }
+                    <div className='ingredient-list'>
+                        {ingredients.map((ingredient) => (
+                            <div className='ingredient' key={ingredient._id}>
+                                <Link to={{
+                                    pathname: `/ingredients/${ingredient.title}`, state: { ingredient }
+                                }}>
+                                    <img className='ingredient-thumbnail' src={`https://www.themealdb.com/images/ingredients/${ingredient.title}.png`} alt='T_T' />
+                                    <div className='ingredient-name'>{ingredient.title}</div>
+                                </Link>
+                                <div className='button-container'>
+                                    <button className='recipe-but' onClick={() => addIngredient(ingredient._id)}>Add</button>
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
-                {nextPage !== null ? (
-                    <div className='button-container '>
-                        <button className='recipe-but more' onClick={moreIngredientsHandler}>More Ingredients</button>
+                        ))}
                     </div>
-                ) : (
-                    <></>
-                )}
-            </div>
-        )}
+                    {nextPage !== null ? (
+                        <div className='button-container '>
+                            <button className='recipe-but more' onClick={moreIngredientsHandler}>More Ingredients</button>
+                        </div>
+                    ) : (
+                        <></>
+                    )}
+                </div>
+            )}
         </>
     );
 }
